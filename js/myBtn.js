ec9047236c73c25f6434971539f9dd6085d26c71
xzdry <289331962@qq.com>
2018-12-11 17:51:47 +0800

    fixed some bug

---
+++ js/myBtn.js
@@ -0,0 +1,29 @@
+
+window.myBtn={
+    boot,
+}
+// document.write("<script language=javascript src='C:/Users/28933/Desktop/biao-class/project/0001-dynamicLoadCss/dynamicLoadCss.js'></script>");
+
+function boot(selec){
+    let url="C:/Users/28933/Desktop/web/biao-project/005-plugin-biaoTable/css/btn.css";
+    changeBtnStyle();
+    dynamicLoadCss(url);
+}
+
+function changeBtnStyle(selec=document){
+    let btn=selec.querySelectorAll('button');
+    btn.forEach(it=>{
+        it.classList.add('my-btn');
+    })
+}
+// dynamicLoadCss(url);
+function dynamicLoadCss(url){
+    var head=document.getElementsByTagName('head')[0];
+    let link=document.createElement('link');
+
+    
+    link.type='text/css';
+    link.rel='stylesheet';
+    link.href=url;
+    head.appendChild(link);
+}
